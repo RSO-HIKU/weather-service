5683885db286e005f7b4f28c11585f3671f38301
akcabra <andraz.klobucar@gmail.com>
2026-01-03 21:20:17 +0100

    HIKU-21: Added keycloak

--- src/main/java/com/hiku/weatherService/controller/WeatherService.java
+++ src/main/java/com/hiku/weatherService/controller/WeatherService.java
@@ -2,8 +2,12 @@ package com.hiku.weatherService.controller;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import javax.annotation.security.DeclareRoles;
+import org.eclipse.microprofile.auth.LoginConfig;
 
-@ApplicationPath("/weather")
+@LoginConfig(authMethod = "MP-JWT")
+@DeclareRoles({"user", "admin"})
+@ApplicationPath("/api/weather")
 public class WeatherService extends Application {
     
 }
